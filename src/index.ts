e8fc6374a0cc611f2c617e66f3c49769f39481cc
Jacob Heric <jacob@jacobheric.com:q!>
2023-07-20 13:47:00 -0400

    update app to be esm compliant and use esm snitch-proto libs

--- src/index.ts
+++ src/index.ts
@@ -1,57 +1,36 @@
-import { credentials } from "@grpc/grpc-js";
+import { ChannelCredentials } from "@grpc/grpc-js";
+import { GrpcTransport } from "@protobuf-ts/grpc-transport";
 import {
   ExternalClient,
   IExternalClient,
-} from "@streamdal/snitch-protos/protos/external_api.grpc-client";
-export const hello = () => "Hello World!";
+} from "@streamdal/snitch-protos/protos/external_api.client.js";
 
-enum Mode {
-  Consume,
-  Publish,
-}
-
-interface Config {
-  dataSource: string;
-  plumberUrl: string;
-  plumberToken: string;
-  dryRun: boolean;
-  wasmTimeout: string; // seconds
-}
-
-export const getConfigs = (): Config => ({
-  dataSource: "",
-  plumberUrl: process.env.PLUMBER_URL || "",
-  plumberToken: process.env.PLUMBER_TOKEN || "",
-  dryRun: !process.env.DATAQUAL_DRY_RUN,
-  wasmTimeout: `${process.env.DATAQUAL_WASM_TIMEOUT || 1}s`,
+const transport = new GrpcTransport({
+  host: "localhost:9091",
+  channelCredentials: ChannelCredentials.createInsecure(),
 });
 
-const client: IExternalClient = new ExternalClient(
-  "http://localhost:9091",
-  credentials.createInsecure(),
-  {},
-  {}
-);
+const client: IExternalClient = new ExternalClient(transport);
 
-const start = () => {
-  const call = client.test({ input: "hello world" }, (err, value) => {
-    if (err) {
-      console.log("got err: ", err);
-    }
-    if (value) {
-      console.log("got response message: ", value);
-    }
-  });
+const start = async () => {
+  const call = client.test(
+    { input: "hello world" },
+    { meta: { "auth-token": "1234" } }
+  );
 
-  call.on("metadata", (arg1) => {
-    console.log("got response headers: ", arg1);
-  });
+  console.log(`### calling method "${call.method.name}"...`);
 
-  call.on("status", (arg1) => {
-    console.log("got status: ", arg1);
-  });
+  const headers = await call.headers;
+  console.log("got response headers: ", headers);
 
-  call.on("status", (status) => console.log("status", status));
+  const response = await call.response;
+  console.log("got response message: ", response);
+
+  const status = await call.status;
+  console.log("got status: ", status);
+
+  const trailers = await call.trailers;
+  console.log("got trailers: ", trailers);
 };
 
-start();
+await start();
