faafeb94c99ea99c7452891343893484dc350864
Jacob Heric <jacob@jacobheric.com:q!>
2023-12-13 10:53:49 -0500

    flesh out kv example

--- src/sandbox/kv.ts
+++ src/sandbox/kv.ts
@@ -2,7 +2,6 @@ import { Audience } from "@streamdal/protos/protos/sp_common";
 
 import { OperationType, Streamdal, StreamdalConfigs } from "../streamdal.js";
 import { loadData } from "./billing.js";
-import { runPipeline } from "./index.js";
 
 const serviceKVConfig: StreamdalConfigs = {
   streamdalUrl: "localhost:8082",
@@ -20,9 +19,6 @@ const KVProducer: Audience = {
   operationName: "import",
 };
 
-const welcome = new Streamdal(serviceKVConfig);
-const wpData = loadData("./src/sandbox/assets/sample-welcome-producer.json");
-
 /**
  * 1. run this
  * 2. go to the console, create a pipeline with a "Key/Value" step type,
@@ -36,10 +32,34 @@ const wpData = loadData("./src/sandbox/assets/sample-welcome-producer.json");
  *      "http://localhost:8081/api/v1/kv"
  */
 export const kv = () => {
+  const kvService = new Streamdal(serviceKVConfig);
+  const kvData = loadData("./src/sandbox/assets/sample-welcome-producer.json");
+
   //
-  // Key exists, this will result in a pipeline step running without error
-  runPipeline(welcome, KVProducer, wpData[0], 1000);
+  // eslint-disable-next-line @typescript-eslint/no-misused-promises
+  setInterval(async () => {
+    const result = await kvService.process({
+      audience: KVProducer,
+      data: new TextEncoder().encode(JSON.stringify(kvData[0])),
+    });
+
+    //
+    // Key exists, this will result in a pipeline step running without error
+    // if this is part of multi-step or multi-pipeline you will need to inspect pipelineStatus
+    console.debug(result.error);
+  }, 1000);
+
   //
-  // Key does not exist, this will result in a pipeline step running without error
-  runPipeline(welcome, KVProducer, wpData[1], 1000);
+  // eslint-disable-next-line @typescript-eslint/no-misused-promises
+  setInterval(async () => {
+    const result = await kvService.process({
+      audience: KVProducer,
+      data: new TextEncoder().encode(JSON.stringify(kvData[1])),
+    });
+
+    //
+    // Key does not exist, this will result in an error
+    // if this is part of multi-step or multi-pipeline you will need to inspect pipelineStatus
+    console.debug(result.error);
+  }, 1000);
 };
